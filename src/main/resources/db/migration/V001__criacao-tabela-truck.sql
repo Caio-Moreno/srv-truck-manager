38ad5753a909697cfc21bed78e65c73f237cef3d
Caio Moreno <cvinicius.moreno@outlook.com>
2023-02-13 20:52:14 -0300

    removendo flyway

--- src/main/resources/db/migration/V001__criacao-tabela-truck.sql
+++
@@ -1,7 +0,0 @@
-create table truck (
-  id bigint not null auto_increment,
-  description varchar(100) null,
-  plate varchar(7) not null,
-
-  primary key (id)
-) engine=InnoDB default charset=utf8;
